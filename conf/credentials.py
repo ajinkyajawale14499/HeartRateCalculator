1565f1a68d5745b96e699fb89b566383daf3451b
Ajinkya <ajinkyajawale14499@gmail.com>
2020-06-11 14:19:42 +0530

    Update credentials.py

--- conf/credentials.py
+++ conf/credentials.py
@@ -3,6 +3,16 @@ This config file would have the credentials
 [default]
 aws_access_key_id = "enter aws id"
 aws_secret_access_key = "enter aws secret access key"
+
+###
+one can find these keys in aws profile in right upper conrner of aws console
+  ->https://console.aws.amazon.com/iam/home#/security_credentials
+  ->Access keys (access key ID and secret access key)
+  
+or just visit aws credentials in your home directory (for linux or mac)
+  cd ~./aws
+  gedit credentials
+  gedit config
 """
 
 #aws
